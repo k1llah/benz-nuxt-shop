63a6df116b10f3263890e854a59c73180316bffd
John Doe <surenbound@yandex.ru>
2024-06-26 03:55:27 +0300

    edit transition

--- app.vue
+++ app.vue
@@ -47,11 +47,11 @@ const colorMode = useColorMode()
 <style>
 .page-enter-active,
 .page-leave-active {
-  transition: all 0.4s;
+  transition: all 0.3s;
 }
 .page-enter-from,
 .page-leave-to {
   opacity: 0;
-  filter: blur(1rem);
+  filter: blur(0.6rem);
 }
 </style>
